0c56aa2479c2438d646e0bb6bba859434cfcc450
Rafael <rateixei@cern.ch>
2016-03-26 14:21:57 +0100

    fixes

--- Gen/GENSIM/crabConfigs/crab10GeV.py
+++ Gen/GENSIM/crabConfigs/crab10GeV.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'GENSIM_GluGluToHToAATo4G_mA_10GeV_Pythia8'
+config.General.requestName = 'GENSIM_GluGluToHToAATo4G_mA_10GeV_13TeV_Pythia8'
 config.General.workArea = '/afs/cern.ch/work/r/rateixei/work/NEU_H4G/Mar23/CMSSW_7_6_1/src/crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
@@ -9,9 +9,9 @@ config.General.transferLogs = False
 config.JobType.pluginName = 'PrivateMC'
 config.JobType.psetName = '/afs/cern.ch/work/r/rateixei/work/NEU_H4G/Mar23/CMSSW_7_6_1/src/Configs/GENSIM_GluGluToHToAATo4G_mA_10GeV_Pythia8.py'
 
-config.Data.outputPrimaryDataset = 'GluGluToHToAATo4G_mA_10GeV_Pythia8'
+config.Data.outputPrimaryDataset = 'GluGluToHToAATo4G_mA_10GeV_13TeV_Pythia8'
 config.Data.splitting = 'EventBased'
-config.Data.unitsPerJob = 10
+config.Data.unitsPerJob = 5000
 NJOBS = 20  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.outLFNDirBase = '/store/user/rateixei/H4G/'
