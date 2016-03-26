9db790794fd61394b3d3f07e3f1bb1ab8df1363a
Rafael <rateixei@cern.ch>
2016-03-26 12:02:46 +0100

    GENSIM

---
+++ Gen/GENSIM/crabConfigs/crab35GeV.py
@@ -0,0 +1,22 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = 'GENSIM_GluGluToHToAATo4G_mA_35GeV_Pythia8'
+config.General.workArea = '/afs/cern.ch/work/r/rateixei/work/NEU_H4G/Mar23/CMSSW_7_6_1/src/crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = '/afs/cern.ch/work/r/rateixei/work/NEU_H4G/Mar23/CMSSW_7_6_1/src/Configs/GENSIM_GluGluToHToAATo4G_mA_35GeV_Pythia8.py'
+
+config.Data.outputPrimaryDataset = 'GluGluToHToAATo4G_mA_35GeV_Pythia8'
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 10
+NJOBS = 20  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
+config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+config.Data.outLFNDirBase = '/store/user/rateixei/H4G/'
+config.Data.publication = True
+config.Data.outputDatasetTag = 'GENSIM-RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12'
+
+#config.Site.storageSite = <site where the user has permission to write>
+config.Site.storageSite = "T2_CH_CERN"
--- Gen/Step0_GluGluToHToXXTo4G-fragment.py
+++ Gen/Step0_GluGluToHToXXTo4G-fragment.py
@@ -5,9 +5,8 @@
 import FWCore.ParameterSet.Config as cms
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
-from Configuration.Generator.Pythia8PowhegEmissionVetoSettings_cfi import *
 
-generator = cms.EDFilter("Pythia8HadronizerFilter",
+generator = cms.EDFilter("Pythia8GeneratorFilter",
                          maxEventsToPrint = cms.untracked.int32(1),
                          pythiaPylistVerbosity = cms.untracked.int32(1),
                          filterEfficiency = cms.untracked.double(1.0),
@@ -16,26 +15,19 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
                          PythiaParameters = cms.PSet(
         pythia8CommonSettingsBlock,
         pythia8CUEP8M1SettingsBlock,
-        pythia8PowhegEmissionVetoSettingsBlock,
         processParameters = cms.vstring(
-            'POWHEG:nFinal = 1',   ## Number of final state particles
-                                   ## (BEFORE THE DECAYS) in the LHE
-                                   ## other than emitted extra parton
+	    'HiggsSM:all = on',
             'Higgs:useBSM = on',
             '25:m0 = 10.',
             '35:m0 = 125.09',
-	    '35:mWidth = 00374',
-           '36:onMode = 0.',
-           '37:onMode = 0.',
             'HiggsBSM:gg2H2  = on',
-	    '25:onMode = 0',
-            '25:onIfAll = 35 35',
             '35:onMode = 0',
-            '35:onIfAll = 22 22'
+            '35:onIfAll = 25 25',
+            '25:onMode = 0',
+            '25:onIfAll = 22 22'
           ),
         parameterSets = cms.vstring('pythia8CommonSettings',
                                     'pythia8CUEP8M1Settings',
-                                    'pythia8PowhegEmissionVetoSettings',
                                     'processParameters'
                                     )
         )
