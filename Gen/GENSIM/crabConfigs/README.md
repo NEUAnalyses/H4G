b550ebbc938c18cf08f83e46361c57b66d45e3f6
amassiro <massironi.andrea@gmail.com>
2016-06-16 06:47:08 +0200

    fix min and max

--- Gen/GENSIM/crabConfigs/README.md
+++ Gen/GENSIM/crabConfigs/README.md
@@ -13,6 +13,9 @@ Crab submission
 
     crab status ../../../crab_projects/crab_GENSIM_GluGluToXToAATo4G_mX_750GeV_mA_370GeV_13TeV_Pythia8_FIX/
 
+    crab status ../../../crab_projects/crab_GENSIM_GluGluToXToAATo4G_mX_750GeV_mA_10GeV_13TeV_Pythia8_FIX_2/
+    crab status ../../../crab_projects/crab_GENSIM_GluGluToXToAATo4G_mX_750GeV_mA_370GeV_13TeV_Pythia8_FIX_2/
+
     
     
 Test generated sample at GEN:
--- Gen/GENSIM/crabConfigs/crab750GeVto370GeV.py
+++ Gen/GENSIM/crabConfigs/crab750GeVto370GeV.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'GENSIM_GluGluToXToAATo4G_mX_750GeV_mA_370GeV_13TeV_Pythia8_FIX'
+config.General.requestName = 'GENSIM_GluGluToXToAATo4G_mX_750GeV_mA_370GeV_13TeV_Pythia8_FIX_2'
 config.General.workArea = '/afs/cern.ch/user/a/amassiro/work/Hgg/H4G/CMSSW_7_6_3_patch2/src/crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
@@ -9,7 +9,7 @@ config.General.transferLogs = False
 config.JobType.pluginName = 'PrivateMC'
 config.JobType.psetName = '//afs/cern.ch/user/a/amassiro/work/Hgg/H4G/CMSSW_7_6_3_patch2/src/H4G/Gen/GENSIM/Configs/GENSIM_GluGluToXToAATo4G_mX_750GeV_mA_10GeV_Pythia8.py'
 
-config.Data.outputPrimaryDataset = 'GluGluToXToAATo4G_mX_750GeV_mA_370GeV_Pythia8_FIX'
+config.Data.outputPrimaryDataset = 'GluGluToXToAATo4G_mX_750GeV_mA_370GeV_Pythia8_FIX_2'
 config.Data.splitting = 'EventBased'
 config.Data.unitsPerJob = 100
 NJOBS = 1000  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
